6a6a16a396a9297024f67854fd0bae52a4755bf0
nunya <nunya@nunya.nunya>
2022-02-11 14:16:06 -0700

    remove comments

--- lib.py
+++ lib.py
@@ -10,93 +10,6 @@ from yaml import load, CLoader
 def get_time():
     return round(1000 * time())
 
-def get_creds():
-    return os.environ.get("TUYA_KEY"), os.environ.get("TUYA_SECRET")
-
-ex_path = "/v1.0/token"
-ex_query = "?grant_type=1"
-ex_method = "GET"
-ex_client_id = "1KAD46OrT9HafiKdsXeg" # from cloud UI
-access_token = "3f4eda2bdec17232f67c0b188af3eec1" # would I have this when getting a token for the "first time"
-ex_secret = "4OHBOnWOqaEC1mWXOpVL3yV50s0qGSRC" # where does this come from?
-ex_req_body = ""
-ex_time = 1588925778000 # would normally be from signature_time()
-ex_nonce = "5138cc3a9033d69856923fd07b491173"
-sig_headers = "area_id:call_id" # how would I know what headers to use? are these http headers or are they parts of a url?
-ex_area_id = "29a33e8796834b1efa6" # I assume this is specific to the users api endpoint
-ex_call_id = "8afdb70ab2ed11eb85290242ac130003" # I also assume this is specific to the api endpoint
-area_id_string = f"area_id:{ex_area_id}"
-call_id_string = f"call_id:{ex_call_id}"
-pgnum = "1"
-page_size = "50"
-
-# def format_request_data(method, request_path, request_query, body="", **required_arguments):
-#     """
-#     formats the data request specific data required by the signature
-#     algorithm as defined in the API docs.
-#
-#     request path is the API endpoint path, e.g.
-#
-#       /v1.0/token
-#
-#     request query is the query part of the request, e.g. (includes the question mark
-#     in this example)
-#
-#       ?grant_type=1
-#
-#     body is the body of the request (form format I think)
-#
-#     required arguments is a dictionary of arguments/parameters required by
-#     the api endpoint, e.g.
-#
-#       {"ara_id": "abc123", "call_id": "456def"}
-#
-#     NB: that dict must be sorted lexicographically based on the keys
-#
-#     :return:
-#     """
-#
-#     body_sha = sha256(body.encode("utf-8")).hexdigest()
-#     url = request_path + request_query
-#     s = sorted(required_arguments.items()) # no sorting function provided as param to sorted func intentionally
-#
-#     t = ""
-#     for k, v in s:
-#         t += f"{k}:{v}" + "\n"
-#
-#     # NB: the format requires TWO new lines after the last param and
-#     # before the url, which is why there's an extra "\n" after appending
-#     # the value of the temporary string variable "t"
-#     tt = method.upper() + "\n" + \
-#          body_sha + "\n" + \
-#          t + "\n" + \
-#          url
-#
-#     return tt
-#
-# def generate_message_to_sign(time, nonce, method, request_path, request_query, body="", **required_arguments):
-#     """
-#     returns the data (string) that is to be signed, which is NOT
-#     the so called "string to sign" mentioned in the API docs, but
-#     it does depend on string-to-sign.
-#
-#     :return:
-#     """
-#     # client_id + sig_time + nonce + sig_string
-#     return ex_client_id + str(time) + nonce + format_request_data(method, request_path, request_query, body, **required_arguments)
-#
-# def gen_sig(message, key):
-#     """
-#     returns the signature data, which depends on
-#
-#     :param message:
-#     :param key:
-#     :return:
-#     """
-#
-#     key_bytes = bytes(key, "utf-8")
-#     encoded_string = message.encode("utf-8")
-#     return hmac.new(key=key_bytes, msg=encoded_string, digestmod='sha256').hexdigest().upper()
 
 class Tuya:
 
@@ -239,15 +152,6 @@ class Tuya:
 
 if __name__ == "__main__":
 
-    # required_args = {
-    #     "call_id": "8afdb70ab2ed11eb85290242ac130003",
-    #     "area_id": "29a33e8796834b1efa6"
-    #
-    # }
-    # m = generate_message_to_sign(ex_time, ex_nonce, ex_method, ex_path, ex_query, ex_req_body, **required_args)
-
     t = Tuya()
-
-    # m = t.generate_message_to_sign(ex_time, ex_nonce, ex_method, ex_path, ex_query, ex_req_body, **required_args)
 
     t.get_auth_token()
